e11df7750c255054ac5c2b06f4cde6144ef92780
mirko <mirko.atanasov@dh.com>
2017-11-10 02:34:27 -0500

    adding summary of all the concepts in the demo

--- src/app/form-validators/form-validators.component.ts
+++ src/app/form-validators/form-validators.component.ts
@@ -3,65 +3,77 @@ import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from
 import { isBlank } from '@angular/core/src/facade/lang';
 import { CustomValidator } from 'app/form-validators/custom-validator';
 
+
+
 @Component({
   selector: 'app-form-validators',
   templateUrl: './form-validators.component.html',
   styleUrls: ['./form-validators.component.css']
 })
 export class FormValidatorsComponent implements OnInit {
-  
-  fg : FormGroup;
+
+  fg: FormGroup;
 
   //nameFormControl : AbstractControl;
 
-  constructor(fb : FormBuilder) 
-  { 
-    var surnameFormControl = fb.control('Pedro',Validators.compose([CustomValidator.startsWithUpperCaseValidator]));
+  constructor(fb: FormBuilder) {
+    //'Pedro' is the inital value for the FormGroup instance that is intended to capture the surname of the user
+    var surnameFormControl = fb.control('Pedro', Validators.compose([CustomValidator.startsWithUpperCaseValidator]));
+
+    //44 is the inital value for the control intended to capture the user's age
     var ageFormControl = fb.control(44, Validators.compose([Validators.required, CustomValidator.mustBeEven]));
 
+    // 'name','age' and 'surname' are the actual control names that will be used in the binding to the template
     this.fg = fb.group({
-      'name':['',Validators.required],
+      'name': ['', Validators.required],
       'age': ageFormControl,
-      'surname':surnameFormControl
+      'surname': surnameFormControl
     })
 
-    //this.nameFormControl = this.fg.controls['name'];
-
-
-    this.fg.controls['name'].valueChanges.subscribe( (nameValue : string) => {
+    //interesting concept of subscription to the event emitted from the formGroup control
+    this.fg.controls['name'].valueChanges.subscribe((nameValue: string) => {
       console.log("name changed to : " + nameValue);
-    } );
+    });
 
-    this.fg.valueChanges.subscribe((formGroupValue : any) => {
+    this.fg.valueChanges.subscribe((formGroupValue: any) => {
       console.log("Form group value: ")
       console.log(formGroupValue);
+      console.log(`this.fg.valid : ${this.fg.valid}`);
     });
+
+
   }
 
 
-  
+  isFormValid(): boolean {
+    //returns true if the form is dirty and valid
+    return (this.fg.dirty && this.fg.valid);
+  }
+
+
+
 
   ngOnInit() {
   }
 
-  OnFormSubmit(formValue:any){
-  console.log(formValue);
-// console.log("aaaaaaaaaaaaaaaaaaaaa")
+  OnFormSubmit(formValue: any) {
+    console.log(formValue);
+    // console.log("aaaaaaaaaaaaaaaaaaaaa")
     //this.fg.errors
 
-    var someString : String;
-    
-    someString =   formValue["surname"];
-var blank = isBlank(someString);
+    var someString: String;
+
+    someString = formValue["surname"];
+    var blank = isBlank(someString);
 
 
 
-console.log(blank);
+    console.log(blank);
 
 
-someString = null;
+    someString = null;
 
-console.log(isBlank(someString));
+    console.log(isBlank(someString));
 
 
   }
